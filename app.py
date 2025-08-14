17b5ba087366650253ef48cc336a85e039d71cab
Kyrollosashraf3 <kerlosashraf33@gmail.com>
2025-08-14 15:55:29 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import joblib
+
+# تحميل الموديل والـ vectorizer
+model = joblib.load("spam_classifier.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+st.title("📧 Spam Email Classifier")
+email_text = st.text_area("أدخل نص الإيميل هنا:")
+
+if st.button("تحليل"):
+    features = vectorizer.transform([email_text])
+    prediction = model.predict(features)
+    st.write("النتيجة:", "🚨 Spam" if prediction[0] == "spam" else "✅ Not Spam")
